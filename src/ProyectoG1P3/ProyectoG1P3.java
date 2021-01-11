e3e0d0d34d602fc2cf47ec3f5ba4c629177c80dd
fElizalde99 <77074497+fElizalde99@users.noreply.github.com>
2021-01-11 15:41:25 -0500

    todo

--- src/ProyectoG1P3/ProyectoG1P3.java
+++ src/ProyectoG1P3/ProyectoG1P3.java
@@ -6,18 +6,35 @@
 package ProyectoG1P3;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+/*import java.io.*;
+import java.util.*;
+import java.lang.*;
+import Enums.*;
+import modelo.*;*/
 
 /**
  *
  * @author Carlos Molineros
  */
-public abstract class ProyectoG1P3 extends Application{
+public class ProyectoG1P3 extends Application{
+    
+    @Override
+    public void start(Stage stage) throws Exception{
+        Parent root= FXMLLoader.load(getClass().getResource("ProyFXML.fxml"));
+        Scene scene= new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        launch(args);
     }
     
 }
