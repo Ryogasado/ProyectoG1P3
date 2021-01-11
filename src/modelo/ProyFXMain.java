583ee9c135b4e44557b2a46fe7530eaffa6bf492
fElizalde99 <77074497+fElizalde99@users.noreply.github.com>
2021-01-11 15:34:19 -0500

    introduccion

---
+++ src/modelo/ProyFXMain.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Favia Elizalde
+ */
+public class ProyFXMain extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        
+        //Botones
+        Button nJuego = new Button();
+        Button Config = new Button();
+        Button Reporte = new Button();
+        
+        //        
+        nJuego.setText("Nuevo Juego");
+        nJuego.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Nuevo Juego");
+            }
+        });
+        
+        Reporte.setText("Reporte");
+        Reporte.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Reporte");
+            }
+        });
+        
+        Config.setText("Configuraciones");
+        Config.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Configuraciones");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(nJuego);
+        root.getChildren().add(Config);
+        root.getChildren().add(Reporte);
+        
+        Scene scene = new Scene(root, 600, 550);
+        
+        primaryStage.setTitle("LOTERIA MEXICANA");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
