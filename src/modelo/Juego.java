b32246e0cd196f33bba939fa3832b944d54511b9
Ryogasado <58095745+Ryogasado@users.noreply.github.com>
2021-01-20 12:18:48 -0500

    correccion

--- src/modelo/Juego.java
+++ src/modelo/Juego.java
@@ -38,43 +38,13 @@ public class Juego {
         boolean verificacion = false;
         switch(alineacion){
             case FILA:
-                for (int i = 0; i < aleatorios.length; i++) {  
-                    int sumf = 0;
-                    for (int j = 0; j < aleatorios[i].length; j++) {
-                        int valor =tblJugador.getMatrizMapa().get(aleatorios[i][j]);
-                        sumf=sumf+ valor;  
-                    }  
-                    if (sumf == 4){
-                        return true;
-                        
-                    } 
-                  }  
+                
                 break;
             case COLUMNA:
-                for (int i = 0; i < aleatorios[0].length; i++) {  
-                    int sumc=0;  
-                    for (int j = 0; j < aleatorios.length; j++) {  
-                        int valor = tblJugador.getMatrizMapa().get(aleatorios[j][i]);
-                        sumc=sumc+valor;
-                        if(sumc == 4){
-                            return true;
-                        }
-                    }
-                }
+                
                 break;
             case ESQUINA:
-                int id1 = aleatorios[0][0];
-                int valor1 = tblJugador.getMatrizMapa().get(id1);
-                int id2 = aleatorios[0][3];
-                int valor2 = tblJugador.getMatrizMapa().get(id2);
-                int id3 = aleatorios[3][0];
-                int valor3 = tblJugador.getMatrizMapa().get(id3);
-                int id4 = aleatorios[3][3];
-                int valor4 = tblJugador.getMatrizMapa().get(id4);
-                int suma = valor1+valor2+valor3+valor4;
-                if (suma==4){
-                    return true;
-                }
+                
 
         }
 
